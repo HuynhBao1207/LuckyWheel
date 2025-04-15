150286a16326fe168202893944b9cc9e732fb56e
HuynhhBao <hbao70018@gmail.com>
2025-04-15 11:21:11 +0700

    db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'mysql';
+$dbname = 'lucky_wheel';
+$username = 'root';
+$password = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Lỗi kết nối: " . $e->getMessage());
+}
+?>
